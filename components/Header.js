2afb1250eb9d509887c12250efdca10c42d9d2c5
Luong Bang <luongbanghtk@gmail.com>
2024-03-08 13:27:19 +1100

    new update

--- components/Header.js
+++ components/Header.js
@@ -15,7 +15,7 @@ function Header() {
       <div className="p-5 pb-3 pl-10 border-b-[4px] border-gray-200 flex items-center justify-between">
         <div className="flex items-center">
           <div className="flex gap-24 items-center">
-            <Image src="/logo.png" width={70} height={70} alt="logo" />
+            <Image src="/logo.png" width={80} height={80} alt="logo" />
             <div className="flex gap-6 items-center">
               {headerMenus.map((item) => (
                 <div className="flex gap-2 items-center" key={item.id}>
